23c3076a70bbfbfd09152746696891daecdb5713
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-11 13:55:06 +0530

    html and css added

--- server.js
+++ server.js
@@ -20,14 +20,18 @@ http.createServer(function(req, res) {
     try {
         stats = fs.lstatSync(fileName);
     } catch (e) {
-        res.writeHead(404, { "Content-type": "text/plain" });
+        res.writeHead(404, {
+            "Content-type": "text/plain"
+        });
         res.write('404 Not Found\n');
         res.end();
         return;
     }
     if (stats.isFile()) {
         var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]];
-        res.writeHead(200, { "Content-Type": mimeType });
+        res.writeHead(200, {
+            "Content-Type": mimeType
+        });
         var fileStream = fs.createReadStream(fileName);
         fileStream.pipe(res);
     } else if (stats.isDirectory()) {
@@ -36,7 +40,9 @@ http.createServer(function(req, res) {
         });
         res.end();
     } else {
-        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.writeHead(500, {
+            "Content-Type": "text/plain"
+        });
         res.write("500 Internal Error\n");
         res.end();
     }
